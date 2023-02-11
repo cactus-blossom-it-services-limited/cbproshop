ac64b7d2f547ed06304ff7f49e60b7965c8ba3c9
andrew farquharson <farquharson.a@gmail.com>
2023-02-11 19:07:39 +0000

    add bootstrap, nav,header,footer

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">CbProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/cart">
+                <i className="fa-solid fa-cart-shopping"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fa-solid fa-user"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
