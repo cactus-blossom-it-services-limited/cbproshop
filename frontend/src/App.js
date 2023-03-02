1483dd16e1f96ce7a6e5a44a5bcd5a5907c7cf00
andrew farquharson <farquharson.a@gmail.com>
2023-03-02 17:36:19 +0000

    install react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -5,15 +6,15 @@ import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
